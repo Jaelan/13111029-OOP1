16af17a3223774857d1dfc32088786170e6be1ad
jaelan sopari <jaelansopari@gmail.com>
2016-04-10 01:19:25 +0700

    Array 1 Demensi

---
+++ Array Sederhana/src/quis/Quis.java
@@ -0,0 +1,332 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quis;
+
+/**
+ *
+ * @author jay
+ */
+public class Quis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String [] soal = new String [250];
+        
+        System.out.println("Soal Serangga dan Hewan!! (IPA)");
+        
+        soal[0] = 1 +".Apa itu hewan herbivora ?";
+        soal[1] =    "a. Pemakan tumbuhan  ";
+        soal[2] =    "b. Pemakan daging";
+        soal[3] =    "c. a dan b benar";
+        soal[4] =    "d. semua benar";
+        
+        soal[5] = 2 +".Apa itu hewan karnivora ?";
+        soal[6] =    "a. Pemakan tumbuhan  ";
+        soal[7] =    "b. Pemakan daging";
+        soal[8] =    "c. a dan b benar";
+        soal[9] =    "d. semua benar";
+        
+        soal[10] = 3 +".Domba termasuk Pemakan ?";
+        soal[11] =    "a. Tumbuhan  ";
+        soal[12] =    "b. Daging";
+        soal[13] =    "c. a dan b benar";
+        soal[14] =    "d. semua benar";
+        
+        soal[15] = 4 +".Ayam termasuk pemakan segala, maka hewan itu termasuk kedalam?";
+        soal[16] =    "a. Omnivora  ";
+        soal[17] =    "b. Karnivora";
+        soal[18] =    "c. Herbivora";
+        soal[19] =    "d. semua benar";
+        
+        soal[20] = 5 +".Hewan yang termasuk mamalia?";
+        soal[21] =    "a. Paus  ";
+        soal[22] =    "b. Ular ";
+        soal[23] =    "c. a dan b benar";
+        soal[24] =    "d. semua benar";
+        
+        soal[25] = 6 +".Hewan yang termasuk bertelur ?";
+        soal[26] =    "a. Ayam ";
+        soal[27] =    "b. Harimau";
+        soal[28] =    "c. a dan b benar";
+        soal[29] =    "d. semua benar";
+        
+        soal[30] = 7 +".Apa itu Hewan Omnivora ?";
+        soal[31] =    "a. Hewan Pemakan Segala  ";
+        soal[32] =    "b. Pemakan daging";
+        soal[33] =    "c. a dan b benar";
+        soal[34] =    "d. semua benar";
+        
+        soal[35] = 8 +".Termasuk hewan apakah monyet ?";
+        soal[36] =    "a. Mamalia  ";
+        soal[37] =    "b. Pemakan daging";
+        soal[38] =    "c. a dan b benar";
+        soal[39] =    "d. semua benar";
+        
+        soal[40] = 9 +".Termasuk hewan apakah kadal ?";
+        soal[41] =    "a. Reptil ";
+        soal[42] =    "b. Amphibi";
+        soal[43] =    "c. Mamalia";
+        soal[44] =    "d. semua benar";
+        
+        soal[45] = 10 +".Ikan bernafas dengan ?";
+        soal[46] =    "a. Insang ";
+        soal[47] =    "b. Trakea";
+        soal[48] =    "c. Paru-paru";
+        soal[49] =    "d. semua benar";
+        
+        soal[50] = 11 +".Paus bernafas dengan ?";
+        soal[51] =    "a. Paru-paru  ";
+        soal[52] =    "b. Insang";
+        soal[53] =    "c. a dan b benar";
+        soal[54] =    "d. semua benar";
+        
+        soal[55] = 12 +".Kodok termasuk hewan ?";
+        soal[56] =    "a. Reptil  ";
+        soal[57] =    "b. Mamalia";
+        soal[58] =    "c. Amphibi";
+        soal[59] =    "d. Melata";
+        
+        soal[60] = 13 +".Belalang bernafas dengan ?";
+        soal[61] =    "a. Trankea  ";
+        soal[62] =    "b. Paru-pau";
+        soal[63] =    "c. a dan b benar";
+        soal[64] =    "d. semua benar";
+        
+        soal[65] = 14 +".Hewan Yang dapat Melahirkan?";
+        soal[66] =    "a. Domba  ";
+        soal[67] =    "b. Ayam";
+        soal[68] =    "c. Buaya";
+        soal[69] =    "d. Cikcak";
+        
+        soal[70] = 15 +".Hewan yang dapat melahirkan dan bertelur adalah?";
+        soal[71] =    "a. Ular";
+        soal[72] =    "b. Ayam";
+        soal[73] =    "c. a dan b benar";
+        soal[74] =    "d. semua benar";
+       
+        soal[75] = 16 +".Kodok bernafas dengan ?";
+        soal[76] =    "a. Insang  ";
+        soal[77] =    "b. Kulit";
+        soal[78] =    "c. Paru-paru";
+        soal[79] =    "d. semua benar";
+        
+        soal[80] = 17 +".Harimau termasuk kedalam hewan ?";
+        soal[81] =    "a. Karnivora ";
+        soal[82] =    "b. Omnivora";
+        soal[83] =    "c. herbivora";
+        soal[84] =    "d. semua benar";
+        
+        soal[85] = 18 +".Kanguru termasuk hewan ?";
+        soal[86] =    "a. Mamalia ";
+        soal[87] =    "b. Amphibi";
+        soal[88] =    "c. a dan b benar";
+        soal[89] =    "d. semua benar";
+        
+        soal[90] = 19 +".Serangga apa yang dapat bermetamorfosis ?";
+        soal[91] =    "a. Ulat";
+        soal[92] =    "b. Ayam";
+        soal[93] =    "c. a dan b benar";
+        soal[94] =    "d. semua benar";
+       
+        soal[95] = 20 +".Gajah termasuk kedalam hewan ?";
+        soal[96] =    "a. Mamalia";
+        soal[97] =    "b. Melata";
+        soal[98] =    "c. a dan b benar";
+        soal[99]=    "d. semua benar";
+        
+        soal[100] = 21 +".Gorila termasuk heawan permakan segala, termasuk hewan apakah itu ?";
+        soal[101] =    "a. Omnivora  ";
+        soal[102] =    "b. Karnivora";
+        soal[103] =    "c. Herbivora";
+        soal[104] =    "d. semua benar";
+        
+        soal[105] = 22 +".Dinamakan Hewan apa yang bisa terbang ?";
+        soal[106] =    "a. Burung";
+        soal[107] =    "b. Reptil";
+        soal[108] =    "c. a dan b benar";
+        soal[109] =    "d. semua benar";
+        
+        soal[110] = 23 +".Dibawah ini yang termasuk hewan beracun ?";
+        soal[111] =    "a. Ular  ";
+        soal[112] =    "b. Komodo";
+        soal[113] =    "c. a dan b benar";
+        soal[114] =    "d. semua benar";
+        
+        soal[115] = 24 +".Gorila hidup dihutan ?";
+        soal[116] =    "a. Tropis ";
+        soal[117] =    "b. hujan";
+        soal[118] =    "c. a dan b benar";
+        soal[119] =    "d. semua benar";
+        
+        soal[120] = 25 +".Gajah melindingi dirinya menggunakan ?";
+        soal[121] =    "a. Gading  ";
+        soal[122] =    "b. Belalai";
+        soal[123] =    "c. Kuping";
+        soal[124] =    "d. semua benar";
+        
+        soal[125] = 26 +".Harimau termasuk kedalam  hewan?";
+        soal[126] =    "a. Mamalia  ";
+        soal[127] =    "b. Amphibi";
+        soal[128] =    "c. Serangga";
+        soal[129] =    "d. semua benar";
+        
+        soal[130] = 27 +".Kangguru termasuk hewan dari negara ?";
+        soal[131] =    "a. Indonesia ";
+        soal[132] =    "b. Amerika";
+        soal[133] =    "c. Australia";
+        soal[134] =    "d. semua benar";
+        
+        soal[135] = 28 +".Elang termasuk pemakan daging, hewan itu disebut  ?";
+        soal[136] =    "a. Herbivora  ";
+        soal[137] =    "b. Karnivora";
+        soal[138] =    "c. Herbivora";
+        soal[139] =    "d. semua benar";
+        
+        soal[140] = 29 +".Kura-kura termasuk kedalam hewan ?";
+        soal[141] =    "a. Amphibi ";
+        soal[142] =    "b. Mamalia";
+        soal[143] =    "c. a dan b benar";
+        soal[144] =    "d. semua benar";
+        
+        soal[145] = 30 +".Pinguin hidung di ?";
+        soal[146] =    "a. Salju  ";
+        soal[147] =    "b. Hutan tropis";
+        soal[148] =    "c. Padang Savana";
+        soal[149] =    "d. semua benar";
+        
+        soal[150] = 31 +".Kucing termasuk kedalam hewan ?";
+        soal[151] =    "a. Herbivora ";
+        soal[152] =    "b. Mamalia";
+        soal[153] =    "c. a dan b benar";
+        soal[154] =    "d. semua benar";
+        
+        soal[155] = 32 +".Dibawah ini yang termasuk Hewan pengerat ?";
+        soal[156] =    "a. Tikus ";
+        soal[157] =    "b. Marmot";
+        soal[158] =    "c. a dan b benar";
+        soal[159] =    "d. semua salah";
+        
+        soal[160] = 33 +".Yang termasuk Hewan yang bertulang belakang ?";
+        soal[161] =    "a. Domba";
+        soal[162] =    "b. Kangguru";
+        soal[163] =    "c. a dan b benar";
+        soal[164] =    "d. semua salah";
+        
+        soal[165] = 34 +".Hewan apa yang dapat berubah warna?";
+        soal[166] =    "a. Bunglon";
+        soal[167] =    "b. Kupu-kupu";
+        soal[168] =    "c. Jerapah";
+        soal[169] =    "d. semua benar";
+        
+        soal[170] = 35 +".Hewan apa yang tercepat didunia ?";
+        soal[171] =    "a. Cheetah ";
+        soal[172] =    "b. Harimau";
+        soal[173] =    "c. kura-kura";
+        soal[174] =    "d. Kelinci";
+        
+        soal[175] = 36 +".Hewan apa yang dapat menangkap glombang ultrasonic?";
+        soal[176] =    "a. Kelelawar  ";
+        soal[177] =    "b. Kucing";
+        soal[178] =    "c. beruang";
+        soal[179] =    "d. semua benar";
+       
+        soal[180] = 37 +".Beruang termasuk kedalam hewan ?";
+        soal[181] =    "a. Mamalia";
+        soal[182] =    "b. Herbifora";
+        soal[183] =    "c. karnivora";
+        soal[184] =    "d. semua benar";
+        
+        soal[185] = 38 +".Hewan apa yang dijuluki dengan Raja Hutan ?";
+        soal[186] =    "a. Cheetah ";
+        soal[187] =    "b. Singa";
+        soal[188] =    "c. a dan b benar";
+        soal[189] =    "d. semua benar";
+        
+        soal[190] = 39 +".Hewan apa yang dapat hidup didarat dan dilaut?";
+        soal[191] =    "a. Mamalia  ";
+        soal[192] =    "b. Amphibi";
+        soal[193] =    "c. Pengerat";
+        soal[194] =    "d. semua benar";
+        
+        soal[195] = 40 +".Ayam berkembangbiak dengan cara ?";
+        soal[196] =    "a. Bertelur ";
+        soal[197] =    "b. Melahirkan";
+        soal[198] =    "c. a dan b benar";
+        soal[199] =    "d. semua benar";
+        
+        soal[200] = 41 +".Nama hewan apa yang memiliki duri ?";
+        soal[201] =    "a. Ular ";
+        soal[202] =    "b. Burung";
+        soal[203] =    "c. Landak";
+        soal[204] =    "d. semua benar";
+        
+        soal[205] = 42 +".Harimau berhabitat di?";
+        soal[206] =    "a. Padang savana";
+        soal[207] =    "b. Hutan tropis";
+        soal[208] =    "c. a dan b benar";
+        soal[209] =    "d. semua benar";
+        
+        soal[210] = 43 +".Nama hewan apa yang memilki tangan terpanjang  ?";
+        soal[211] =    "a. Monyet OA  ";
+        soal[212] =    "b. Lutung";
+        soal[213] =    "c. Gorilla";
+        soal[214] =    "d. semua benar";
+        
+        soal[215] = 44 +".Hewan apa yang memiliki jaring ?";
+        soal[216] =    "a. Laba-laba ";
+        soal[217] =    "b. Kucing";
+        soal[218] =    "c. a dan b benar";
+        soal[219] =    "d. semua benar";
+        
+        soal[220] = 45 +".Kelinci berkembang biak dengan cara?";
+        soal[221] =    "a. Melahirkan  ";
+        soal[222] =    "b. Bertelur";
+        soal[223] =    "c. Melahirkan dan bertelur";
+        soal[224] =    "d. semua benar";
+        
+        soal[225] = 46 +".Burung Elang berkembang biak dengan cara ?";
+        soal[226] =    "a. Bertelur";
+        soal[227] =    "b. Melahirkan";
+        soal[228] =    "c. a dan b benar";
+        soal[229] =    "d. semua benar";
+        
+        soal[230] = 47 +".Ikan berkembang biak dengan cara ?";
+        soal[231] =    "a. Malahirkan ";
+        soal[232] =    "b. Bertelur";
+        soal[233] =    "c. Melahirkan dan bertelur";
+        soal[234] =    "d. semua benar";
+        
+        soal[235] = 48 +".Rusa melindugi dirinya dengan?";
+        soal[236] =    "a. Tanduk ";
+        soal[237] =    "b. Kaki";
+        soal[238] =    "c. a dan b benar";
+        soal[239] =    "d. kulit";
+        
+        soal[240] = 49 +".Burung Merak berkembang biak dengan cara ?";
+        soal[241] =    "a. melahirkan  ";
+        soal[242] =    "b. Pemakan daging";
+        soal[243] =    "c. a dan b benar";
+        soal[244] =    "d. bertelur";
+         
+        soal[245] = 50 +".Semut bernafas dengan?";
+        soal[246] =    "a. Trakea ";
+        soal[247] =    "b. insang";
+        soal[248] =    "c. a dan b salah";
+        soal[249] =    "d. semua benar";
+         
+        
+        for (int i = 0; i < 250; i++) 
+        {
+          
+            System.out.println(soal[i]);
+               
+        }
+      
+    }
+    
+}
